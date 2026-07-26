3cf826c46989b526a7be86dbd32d3858fe2bc344
Natsukiplayz <natsukiplayzzz@gmail.com>
2026-07-26 16:30:05 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+from threading import Thread
+import subprocess
+
+# Start Flask Ludo server
+def run_web():
+    subprocess.run(["python", "web_server.py"])
+
+Thread(target=run_web, daemon=True).start()
+
+# Start Telegram bot
+subprocess.run(["python", "aru.py"])
